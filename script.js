f70622506d848de7c7c6196a41e700d5c7f7d535
Aman Kumar <125535132+Aman13052002@users.noreply.github.com>
2026-05-29 01:50:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,107 +1,35 @@
-// your JS code here
+//Your code goes here
 
-const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
+const speed = document.querySelector('.speed');
+const bar = document.querySelector('.speed-bar');
+const video = document.querySelector('.flex');
 
-const progress = player.querySelector('.progress');
-const progressFilled =
-  player.querySelector('.progress__filled');
+function handleUpdate(e) {
 
-const toggle =
-  player.querySelector('.toggle');
+  const y = e.pageY - speed.offsetTop;
 
-const ranges =
-  player.querySelectorAll('.player__slider');
+  const percent = y / speed.offsetHeight;
 
-const skipButtons =
-  player.querySelectorAll('[data-skip]');
+  const min = 0.4;
+  const max = 4;
 
+  const playbackRate =
+    percent * (max - min) + min;
 
-// Play / Pause toggle
-function togglePlay() {
+  const height =
+    Math.round(percent * 100) + '%';
 
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
+  // Update bar height
+  bar.style.height = height;
+
+  // Update text
+  bar.textContent =
+    playbackRate.toFixed(2) + '×';
+
+  // Update video speed
+  video.playbackRate = playbackRate;
 }
 
-// Change button icon
-function updateButton() {
-
-  if (video.paused) {
-    toggle.textContent = '►';
-  } else {
-    toggle.textContent = '❚ ❚';
-  }
-}
-
-// Update progress bar
-function handleProgress() {
-
-  const percent =
-    (video.currentTime / video.duration) * 100;
-
-  progressFilled.style.flexBasis =
-    `${percent}%`;
-}
-
-// Scrub progress bar
-function scrub(e) {
-
-  const scrubTime =
-    (e.offsetX / progress.offsetWidth) *
-    video.duration;
-
-  video.currentTime = scrubTime;
-}
-
-// Skip video
-function skip() {
-
-  video.currentTime +=
-    parseFloat(this.dataset.skip);
-}
-
-// Handle volume and playback speed
-function handleRangeUpdate() {
-
-  video[this.name] = this.value;
-}
-
-
-// Event Listeners
-video.addEventListener('click', togglePlay);
-
-video.addEventListener('play', updateButton);
-
-video.addEventListener('pause', updateButton);
-
-video.addEventListener(
-  'timeupdate',
-  handleProgress
-);
-
-toggle.addEventListener('click', togglePlay);
-
-skipButtons.forEach(button =>
-  button.addEventListener('click', skip)
-);
-
-ranges.forEach(range =>
-  range.addEventListener(
-    'change',
-    handleRangeUpdate
-  )
-);
-
-ranges.forEach(range =>
-  range.addEventListener(
-    'mousemove',
-    handleRangeUpdate
-  )
-);
-
-// Progress bar click
-progress.addEventListener('click', scrub);
+// Similar to your example
+speed.addEventListener('mousemove', handleUpdate);
+speed.addEventListener('change', handleUpdate);
